d7aa52a29e37683bd9071646381f0d19150a3634
duridev <duridev@outlook.com>
2024-01-09 22:56:27 -0300

    Creando proptotype para mostrar un error

--- js/app.js
+++ js/app.js
@@ -33,11 +33,18 @@ UI.prototype.mostrarMensaje = (mensaje, tipo) => {
         div.classList.add('error')
     } else {
         div.classList.add('correcto');
-        div.textContent = mensaje;
-
-        // Insertar en el HTML
-        const formulario = document.querySelector('#cotizar-seguro');
     };
+
+    div.classList.add('mensaje', 'mt-10')
+    div.textContent = mensaje;
+
+    // Insertar en el HTML
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.insertBefore(div, document.querySelector('#resultado'));
+
+    setTimeout(() => {
+        div.remove();
+    }, 3000);
 };
 
 // Instanciar UI
@@ -63,13 +70,14 @@ function cotizarSeguro(e) {
         const year = document.querySelector('#year').value;
 
     // Leer el tipo de cobertura (de tipo radio)
-    const tipo = document.querySelector('input[name="tipo"]:cheked').value;
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
 
-    if( marca === '' || year === '' || tipo === || ) {
-
-    } else {
-        
+    if( marca === '' || year === '' || tipo === '' ) {
+        ui.mostrarMensaje('Todos los campos son obligatorios', 'error');
+        return;
     }
 
-    console.log('Cotizando...');
+    ui.mostrarMensaje('Cotizando...', 'exito');
+
+    //
 }
